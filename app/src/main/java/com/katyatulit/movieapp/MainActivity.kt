d6d3979ab4ce79a6fd5a242a25667f69f8c6fd36
KatyaTulit <ek.stupina@gmail.com>
2021-12-25 21:59:27 -0500

    set OnClickListeners for buttons - show toasts with button texts

--- app/src/main/java/com/katyatulit/movieapp/MainActivity.kt
+++ app/src/main/java/com/katyatulit/movieapp/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.katyatulit.movieapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.katyatulit.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        initButtons() // show toast messages when buttons are pressed
+    }
+
+    private fun initButtons() {
+        val buttons = listOf(binding.buttonMenu,
+                            binding.buttonFav,
+                            binding.buttonLater,
+                            binding.buttonSelections,
+                            binding.buttonSettings)
+
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                Toast.makeText(this, button.text.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
